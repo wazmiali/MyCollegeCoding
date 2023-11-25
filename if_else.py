66e542210932f9184181534de0f3e42bbba1b6f5
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2023-11-25 22:23:57 +0530

    python
    python

---
+++ if_else.py
@@ -0,0 +1,38 @@
+# if elif ladder in python
+# a = 8
+# if(a<3): 
+#     print("The value of a is greater than 3")
+# elif(a>13):
+#     print("The value of a is greater than 13")
+# elif(a>7):
+#     print("The value of a is greater than 7")
+# elif(a>17):
+#     print("The value of a is greater than 17")
+# else: 
+#     print("The value is not greater than 3 or 7")
+
+# print("Done")
+
+# Multiple if statements
+
+# a = 8
+# if(a<3): 
+#     print("The value of a is greater than 3")
+# if(a>13):
+#     print("The value of a is greater than 13")
+# if(a>7):
+#     print("The value of a is greater than 7")
+# if(a>17):
+#     print("The value of a is greater than 17")
+# else: 
+#     print("The value is not greater than 3 or 7")
+
+# print("Done")
+
+# Example
+a = 22
+if(a>9):
+    print("Greater")
+else:
+    print("lesser")
+
