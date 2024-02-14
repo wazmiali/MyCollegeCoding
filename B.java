ea6e9da2749b89203ca69f18bc55661789d303b8
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2024-02-14 19:13:11 +0530

    Java basic program examples
    This is a java class examples.

---
+++ B.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class A{
+      void show(){
+            System.out.println("Hello");
+      }         
+}
+public class B{
+      public static void main(String[] args){
+            A obj = new A();
+            obj.show();
+      }
+}
---
+++ Myclass.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class MyClass{
+      
+      private int myField;
+  
+      public MyClass(int initialValue) {
+          this.myField = initialValue;
+      }
+  
+      public void doSomething() {
+        
+      }
+  
+      public int getMyField() {
+          return this.myField;
+      }
+  }
+  
